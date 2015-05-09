7484d696bcc22b03387468583860df4771f2b1ed
Daniel Hess <dan9186@gmail.com>
2015-05-08 23:45:49 -0700

    initial commit

---
+++ bogus.go
@@ -0,0 +1,45 @@
+package bogus
+
+import (
+	"net"
+	"net/http"
+	"net/http/httptest"
+)
+
+type Bogus struct {
+	server  *httptest.Server
+	hits    int
+	payload string
+	status  int
+}
+
+func New() *Bogus {
+	return &Bogus{}
+}
+
+func (b *Bogus) Hits() int {
+	return b.hits
+}
+
+func (b *Bogus) HostPort() (string, string) {
+	h, p, _ := net.SplitHostPort(b.server.URL[7:])
+	return h, p
+}
+
+func (b *Bogus) SetPayload(r string) {
+	b.payload = r
+}
+
+func (b *Bogus) SetStatus(s int) {
+	b.status = s
+}
+
+func (b *Bogus) Start() {
+	b.server = httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(b.status)
+				b.hits++
+				w.Write([]byte(b.payload))
+			}))
+}
