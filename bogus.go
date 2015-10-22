712b253ade19409f822add033e1271a998c899ce
Daniel Hess <dan9186@gmail.com>
2015-10-22 14:18:40 -0700

    adding ability for multiple paths and returns

--- bogus.go
+++ bogus.go
@@ -7,18 +7,40 @@ import (
 )
 
 type Bogus struct {
-	server  *httptest.Server
-	hits    int
+	server *httptest.Server
+	hits   int
+	paths  map[string]*Path
+}
+
+type Path struct {
 	payload []byte
 	status  int
 }
 
 func New() *Bogus {
-	return &Bogus{}
+	return &Bogus{paths: map[string]*Path{
+		"/": &Path{},
+	}}
+}
+
+func (b *Bogus) AddPath(path string) *Path {
+	if _, ok := b.paths[path]; !ok {
+		b.paths[path] = &Path{}
+	}
+
+	return b.paths[path]
 }
 
 func (b *Bogus) Close() {
 	b.server.Close()
+}
+
+func (b *Bogus) HandlePaths(w http.ResponseWriter, r *http.Request) {
+	if path, ok := b.paths[r.URL.Path]; ok {
+		w.WriteHeader(path.status)
+		b.hits++
+		w.Write(path.payload)
+	}
 }
 
 func (b *Bogus) Hits() int {
@@ -31,19 +53,29 @@ func (b *Bogus) HostPort() (string, string) {
 }
 
 func (b *Bogus) SetPayload(p []byte) {
-	b.payload = p
+	path := b.paths["/"]
+	if path != nil {
+		path.payload = p
+	}
+}
+
+func (p *Path) SetPayload(payload []byte) *Path {
+	p.payload = payload
+	return p
 }
 
 func (b *Bogus) SetStatus(s int) {
-	b.status = s
+	path := b.paths["/"]
+	if path != nil {
+		path.status = s
+	}
+}
+
+func (p *Path) SetStatus(s int) *Path {
+	p.status = s
+	return p
 }
 
 func (b *Bogus) Start() {
-	b.server = httptest.NewServer(
-		http.HandlerFunc(
-			func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(b.status)
-				b.hits++
-				w.Write([]byte(b.payload))
-			}))
+	b.server = httptest.NewServer(http.HandlerFunc(b.HandlePaths))
 }
--- bogus_test.go
+++ bogus_test.go
@@ -16,13 +16,9 @@ func TestBogus(t *testing.T) {
 
 	g.Describe("Bogus Functions", func() {
 		var server *Bogus
-		var payload = "some return payload"
-		var status = 200
 
 		g.BeforeEach(func() {
 			server = New()
-			server.SetPayload([]byte(payload))
-			server.SetStatus(status)
 			server.Start()
 		})
 
@@ -36,7 +32,29 @@ func TestBogus(t *testing.T) {
 			Expect(port).ToNot(Equal(""))
 		})
 
-		g.It("should allow setting the payload", func() {
+		g.It("should count hits against the server", func() {
+			host, port := server.HostPort()
+
+			resp, err := http.Get("http://" + net.JoinHostPort(host, port))
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+			Expect(server.Hits()).To(Equal(1))
+		})
+	})
+
+	g.Describe("Paths", func() {
+		var server *Bogus
+		var payload = "some return payload"
+		var status = http.StatusOK
+
+		g.BeforeEach(func() {
+			server = New()
+			server.SetPayload([]byte(payload))
+			server.SetStatus(status)
+			server.Start()
+		})
+
+		g.It("should allow setting the payload for the root path", func() {
 			host, port := server.HostPort()
 
 			resp, err := http.Get("http://" + net.JoinHostPort(host, port))
@@ -49,7 +67,7 @@ func TestBogus(t *testing.T) {
 			Expect(server.Hits()).To(Equal(1))
 		})
 
-		g.It("should allow setting the return status", func() {
+		g.It("should allow setting the return status for the root path", func() {
 			host, port := server.HostPort()
 
 			resp, err := http.Get("http://" + net.JoinHostPort(host, port))
@@ -60,13 +78,45 @@ func TestBogus(t *testing.T) {
 			Expect(server.Hits()).To(Equal(1))
 		})
 
-		g.It("should count hits against the server", func() {
+		g.It("should allow adding a new path", func() {
 			host, port := server.HostPort()
+			server.AddPath("/foo/bar").SetStatus(http.StatusCreated)
 
 			resp, err := http.Get("http://" + net.JoinHostPort(host, port))
 			Expect(err).NotTo(HaveOccurred())
 			defer resp.Body.Close()
+
+			Expect(resp.StatusCode).To(Equal(status))
 			Expect(server.Hits()).To(Equal(1))
+
+			resp, err = http.Get("http://" + net.JoinHostPort(host, port) + "/foo/bar")
+			Expect(err).NotTo(HaveOccurred())
+
+			Expect(resp.StatusCode).To(Equal(http.StatusCreated))
+			Expect(server.Hits()).To(Equal(2))
+		})
+
+		g.It("should return unique payloads per path", func() {
+			host, port := server.HostPort()
+			fooload := "foobar"
+			server.AddPath("/foo/bar").SetPayload([]byte(fooload))
+
+			resp, err := http.Get("http://" + net.JoinHostPort(host, port))
+			Expect(err).NotTo(HaveOccurred())
+			defer resp.Body.Close()
+
+			body, err := ioutil.ReadAll(resp.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(body)).To(Equal(payload))
+			Expect(server.Hits()).To(Equal(1))
+
+			resp, err = http.Get("http://" + net.JoinHostPort(host, port) + "/foo/bar")
+			Expect(err).NotTo(HaveOccurred())
+
+			body, err = ioutil.ReadAll(resp.Body)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(string(body)).To(Equal(fooload))
+			Expect(server.Hits()).To(Equal(2))
 		})
 	})
 }
